dac25fc518b05bb9efa8fe5ac6325da753ffee3f
art2cool <mr.patriotuk@gmail.com>
2015-08-21 18:18:12 +0300

    create number add, clear, and backstep

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ app.controller('calcCtrl', ['$scope', function ($scope) {
 	$scope.result = '';
 
 	$scope.addNumb = function (index) {
-		
+//00000
+//.....
+//0.050...0.
+//max size=23
+
+
 		$scope.result += index;
-		console.log($scope.result);
+		console.log(Number($scope.result));
+
 	};
 
 	$scope.clear = function () {
 		$scope.result = '';
 	}
 
-
+	$scope.backNumb = function (){
+		$scope.result = $scope.result.slice(0,-1);
+		}
 }]);
